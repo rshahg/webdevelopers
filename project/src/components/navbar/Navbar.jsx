405b318ca47ffb6ce8eaff4a3635be0f2470f16b
rshahg <168731272+rshahg@users.noreply.github.com>
2024-11-09 19:36:17 +0500

    Update Navbar.jsx

--- project/src/components/navbar/Navbar.jsx
+++ project/src/components/navbar/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
           <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <a href="#" className="block py-2 px-1 font-bold " aria-current="page">Capabilities</a>
+                <a href="#" className="block text-white py-2 px-1 font-bold " aria-current="page">Capabilities</a>
               </li>
               <li>
                 <a href="#" className="block text-white py-2 px-1 font-bold ">Our Portfolio</a>
