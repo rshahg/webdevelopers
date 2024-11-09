900a2b1f1ef29168736bf049e7e017a2a11d1bfb
rshahg <168731272+rshahg@users.noreply.github.com>
2024-11-09 19:29:28 +0500

    Update App.jsx

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
 import './index.css';
+import Navbar from './components/navbar/Navbar.jsx'
+// import Queto from './pages/getAqueto/Queto.jsx'
+import Footer from './components/footer/Footer.jsx'
 
 function App() {
   return (
     <div>
+      <Navbar />
+      {/* <Queto /> */}
       <HomePage />
+      <Footer/>
     </div>
   );
 }
